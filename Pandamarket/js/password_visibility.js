aea0212257cfddb5de278ab4e23986bc6295af16
thcjfd <thcjfd@naver.com>
2026-07-16 10:05:30 +0900

    modify password visbility toggle

---
+++ Pandamarket/js/password_visibility.js
@@ -0,0 +1,22 @@
+const visibilityButtonList = document.querySelectorAll(".visibility-button");
+
+function toggleVisibility(inputElement, imgElement) {
+  if (inputElement.type === "password") {
+    inputElement.type = "text";
+    imgElement.src = "/resources/login/pswd_visibility_on.png";
+  } else {
+    inputElement.type = "password";
+    imgElement.src = "/resources/login/pswd_visibility_off.png";
+  }
+}
+
+visibilityButtonList.forEach((button) => {
+  const img = button.querySelector("img");
+  const input = button
+    .closest(".password-input")
+    .querySelector(".password-input-field");
+
+  button.addEventListener("click", () => {
+    toggleVisibility(input, img);
+  });
+});
