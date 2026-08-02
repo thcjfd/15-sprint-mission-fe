1b6412db3a574d28c9ac2d157a1be4134143d84e
thcjfd <thcjfd@naver.com>
2026-08-02 12:39:13 +0900

    Add getArticle()

--- Pandamarket/js/ArticleService.js
+++ Pandamarket/js/ArticleService.js
@@ -22,6 +22,25 @@ function getArticleList({ page = 1, pageSize = 10, keyword = "" } = {}) {
     });
 }
 
+function getArticle(id) {
+  return fetch(`${ARTICLE_URL}/${id}`, {
+    method: "GET",
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`게시글 조회 실패! ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      return data;
+    })
+    .catch((error) => {
+      console.log(error.message);
+    });
+}
+
 function createArticle({ title, content, image } = {}) {
   return fetch(ARTICLE_URL, {
     method: "POST",
@@ -62,8 +81,28 @@ function deleteArticle(id) {
     .catch((error) => console.log(error.message));
 }
 
+function patchArticle(id, { title, content, image } = {}) {
+  return fetch(`${ARTICLE_URL}/${id}`, {
+    method: "PATCH",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ title, content, image }),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`게시글 수정 실패:${response.status}`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(`게시글 수정 성공! id: ${data.id}`);
+    })
+    .catch((error) => console.log(error.message));
+}
+
 const testArticle = {
   title: "제목",
   content: "내용",
   image: "https://example.com/test.jpg",
 };
+
+getArticle(6776);
