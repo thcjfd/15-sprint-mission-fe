cb794c12ea892d7666edf3bf2d1de838587e96ac
thcjfd <thcjfd@naver.com>
2026-08-02 14:05:35 +0900

    Add createProduct()&patchProduct()&deleteProduct()

--- Pandamarket/js/ProductService.js
+++ Pandamarket/js/ProductService.js
@@ -34,3 +34,68 @@ export async function getProductList({
     console.log(error.message);
   }
 }
+
+export async function createProduct({
+  name,
+  description,
+  price,
+  tags,
+  images,
+} = {}) {
+  try {
+    const response = await fetch(PRODUCT_URL, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ name, description, price, tags, images }),
+    });
+    if (!response.ok) {
+      throw new Error(`상품 정보 생성 실패! ${response.status}`);
+    }
+    const data = await response.json();
+    console.log(`상품 정보 생성 성공! id:${data.id}`);
+    return data;
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+export async function patchProduct(id,{
+  name,
+  description,
+  price,
+  tags,
+  images,
+} = {} ){
+    try{
+    const response = await fetch(`${PRODUCT_URL}/${id}`, {
+        method: 'PATCH',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({ name, description, price, tags, images }),
+    });
+    if(!response.ok){
+        throw new Error(`상품 정보 수정 실패: ${response.status}`);
+    }
+    const data = await response.json();
+    console.log(`상품 정보 수정 성공! ${data.id}`);
+    return data;
+    }
+    catch(error){
+        console.log(error.message);
+    }
+}
+
+export async function deleteProduct(id) {
+  try {
+    const response = await fetch(`${PRODUCT_URL}/${id}`, {
+      method: "DELETE",
+    });
+    if (!response.ok) {
+      throw new Error(`상품 삭제 실패: ${response.status}`);
+    }
+    const data = await response.json();
+    console.log(`상품 삭제 성공! id: ${data.id}`);
+    return data;
+  } catch (error) {
+    console.log(error.message);
+  }
+}
--- Pandamarket/js/main.js
+++ Pandamarket/js/main.js
@@ -11,3 +11,11 @@ const testArticle = {
   content: "내용",
   image: "https://example.com/test.jpg",
 };
+
+const testProduct = {
+    name: "이름",
+    description: "상품 설명",
+    price: 1000,
+    tags: ['상품'],
+    images: ["https://example.com/test.jpg"],
+};
